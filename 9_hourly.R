002a60ff7dc06178c80a7bb3c0237b2e5e6e887a
Yogesh sahu <73738048+yogass09@users.noreply.github.com>
2024-07-08 18:17:24 +0530

    Update 9_hourly.R

--- 9_hourly.R
+++ 9_hourly.R
@@ -20,17 +20,9 @@ coin_list_all <- crypto_listings(
 )
 
 
-# Assuming coin_list_all is already loaded in your R environment
+# Dataframe 9: Ranks 801-900
+df9 <- coin_list_all %>% filter(cmc_rank >= 801 & cmc_rank <= 900)
 
-# Dataframes for ranks 1-100, 101-200, ..., 901-1000
-dfs <- list()
-
-for (i in 0:9) {
-  start_rank <- i * 100 + 1
-  end_rank <- (i + 1) * 100
-  df_name <- paste0("df", i + 1)
-  dfs[[df_name]] <- coin_list_all %>% filter(cmc_rank >= start_rank & cmc_rank <= end_rank)
-}
 
 
 ##
